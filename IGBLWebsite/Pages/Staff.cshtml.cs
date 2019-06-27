39e550ed87e0e8fa432c3eaa2acf9196ba77042d
Jovan Stanojlovic <jovan.stanojlovic@kt.net>
2019-06-27 14:53:06 -0400

    Added the solution and project files.
    Added EF Core as a dependency.

---
+++ IGBLWebsite/Pages/Staff.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IGBLWebsite.Pages
+{
+    public class StaffModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
